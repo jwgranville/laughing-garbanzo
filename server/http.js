f9ae6637fc5f83cff2fffcc724451c51a4cf3d8a
Joe Granville <874605+jwgranville@users.noreply.github.com>
2026-03-10 12:02:58 -0700

    refactor(client): use shared domain-events module instead of duplicated constants

--- server/http.js
+++ server/http.js
@@ -1,6 +1,6 @@
 /**
  * @author Joe Granville
- * @date 2026-03-09T22:49:03+00:00
+ * @date 2026-03-10T18:38:58+00:00
  * @license MIT
  * @version 0.1.0
  * @email 874605+jwgranville@users.noreply.github.com
@@ -13,5 +13,6 @@ import path from 'path';
 export function createHTTPServer({ projectRoot }) {
   const app = express();
   app.use(express.static(path.join(projectRoot, 'public')));
+  app.use('/shared', express.static(path.join(projectRoot, 'shared')));
   return app;
 }
